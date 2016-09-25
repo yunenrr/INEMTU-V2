f6c960cd9f88dceee5fc1d6a3acd84c93c04dac4
Yunen Ramos Ramírez <yunenrr@gmail.com>
2016-09-25 14:08:38 -0600

    Actualizados por Brayan

--- insertImagen.php
+++ insertImagen.php
@@ -3,12 +3,13 @@ $hostname="163.178.107.130";
 $database ="INEMTU";
 $username ="sqlserver";
 $password ="saucr.12";
+
 $conn = mssql_connect($hostname,$username,$password);
 mssql_select_db($database ,$conn);
 
 $imagen=$_POST['imagen'];
 
-mssql_query("insert into imagen(ruta) values ('pictures/".$imagen."'),$link");
+mssql_query("insert into imagen(ruta) values ('pictures/".$imagen."'),$conn");
 
 mssql_close($conn);
 
--- upload.php
+++ upload.php
@@ -1,5 +1,5 @@
 <?PHP
 $ruta = "pictures/" .basename($_FILES['fotoUp']['name']);
-if(move_uploaded_file($_FILES['fotoUp']['tmp_name'], $ruta))
+if(move_uploaded_file($_FILES['fotoUp']['tmp_name'], $ruta)){
        chmod ("uploads/".basename( $_FILES['fotoUp']['name']), 0644);
-?>
+}
